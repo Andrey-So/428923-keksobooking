20e44ef638edba10d1297b65cf75a75b7d841a5e
Andrey Sorokin <mail@andrey.so>
2017-12-11 20:48:04 +0600

    Начало создания data.js

--- js/map.js
+++ js/map.js
@@ -31,83 +31,83 @@ var ROOMS = {
 };
 
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+//
+// function getRandomElement(array) {
+//   var rnd = Math.floor(Math.random() * array.length);
+//   var ret = array[rnd];
+//   array.splice(rnd, 1);
+//   return ret;
+// }
+//
+// function getFeaturesMap(item) {
+//   if (Math.round(Math.random())) {
+//     return item;
+//   }
+//   return null;
+// }
+//
+// var getFeatures = function () {
+//   return FEATURES.map(getFeaturesMap);
+// };
+//
+// var getAnnouncement = function () {
+//   var x = getRandomInt(300, 900);
+//   var y = getRandomInt(100, 500);
+//   var price = getRandomInt(1000, 1000000);
+//   var rooms = getRandomInt(1, 5);
+//   var guests = getRandomInt(1, 10);
+//   var checkin = getRandomInt(12, 14) + ':00';
+//   var checkout = getRandomInt(12, 14) + ':00';
+//   var typeAndTitle = getRandomElement(TITLES);
+//   var imageDigit = getRandomElement(IMAGES);
+//   var features = getFeatures();
+//   return {
+//     author: {
+//       avatar: 'img/avatars/user' + imageDigit + '.png'
+//     },
+//     offer: {
+//       title: typeAndTitle[0],
+//       address: x + ', ' + y,
+//       price: price,
+//       type: typeAndTitle[1],
+//       rooms: rooms,
+//       guests: guests,
+//       checkin: checkin,
+//       checkout: checkout,
+//       features: features,
+//       description: '',
+//       photos: []
+//     },
+//     location: {
+//       x: x,
+//       y: y
+//     }
+//   };
+// };
+//
+// var getAnnouncements = function (thisCount) {
+//   var announcements = [];
+//   for (var i = 0; i < thisCount; i++) {
+//     announcements.push(getAnnouncement());
+//   }
+//   return announcements;
+// };
+//
+// var getPin = function (i) {
+//   var pin = document.createElement('button');
+//   var data = announcements[i];
+//   pin.className = 'map__pin';
+//   pin.style.left = data.location.x - 20 + 'px';
+//   pin.style.top = data.location.y + 40 + 'px';
+//   pin.innerHTML = '<img src=\"' + data.author.avatar + '\" width=\"40\" height=\"40\" draggable=\"false\">';
+//   pin.id = i;
+//   return pin;
+// };
 
-function getRandomElement(array) {
-  var rnd = Math.floor(Math.random() * array.length);
-  var ret = array[rnd];
-  array.splice(rnd, 1);
-  return ret;
-}
-
-function getFeaturesMap(item) {
-  if (Math.round(Math.random())) {
-    return item;
-  }
-  return null;
-}
-
-var getFeatures = function () {
-  return FEATURES.map(getFeaturesMap);
-};
-
-var getAnnouncement = function () {
-  var x = getRandomInt(300, 900);
-  var y = getRandomInt(100, 500);
-  var price = getRandomInt(1000, 1000000);
-  var rooms = getRandomInt(1, 5);
-  var guests = getRandomInt(1, 10);
-  var checkin = getRandomInt(12, 14) + ':00';
-  var checkout = getRandomInt(12, 14) + ':00';
-  var typeAndTitle = getRandomElement(TITLES);
-  var imageDigit = getRandomElement(IMAGES);
-  var features = getFeatures();
-  return {
-    author: {
-      avatar: 'img/avatars/user' + imageDigit + '.png'
-    },
-    offer: {
-      title: typeAndTitle[0],
-      address: x + ', ' + y,
-      price: price,
-      type: typeAndTitle[1],
-      rooms: rooms,
-      guests: guests,
-      checkin: checkin,
-      checkout: checkout,
-      features: features,
-      description: '',
-      photos: []
-    },
-    location: {
-      x: x,
-      y: y
-    }
-  };
-};
-
-var getAnnouncements = function (thisCount) {
-  var announcements = [];
-  for (var i = 0; i < thisCount; i++) {
-    announcements.push(getAnnouncement());
-  }
-  return announcements;
-};
-
-var getPin = function (i) {
-  var pin = document.createElement('button');
-  var data = announcements[i];
-  pin.className = 'map__pin';
-  pin.style.left = data.location.x - 20 + 'px';
-  pin.style.top = data.location.y + 40 + 'px';
-  pin.innerHTML = '<img src=\"' + data.author.avatar + '\" width=\"40\" height=\"40\" draggable=\"false\">';
-  pin.id = i;
-  return pin;
-};
-
-var announcements = getAnnouncements(DEFAULT_COUNT);
+// var announcements = window.data.getAnnouncements(DEFAULT_COUNT);
 
 var createMapInfo = function (i) {
   var template = document.getElementsByTagName('template');
@@ -152,7 +152,7 @@ var mapInfoClose = function (evt) {
 
 var showPins = function () {
   for (var i = 0; i < DEFAULT_COUNT; i++) {
-    var mapPin = getPin(i);
+    var mapPin = window.data.getPin(i);
     mapPins.appendChild(mapPin);
   }
 };
